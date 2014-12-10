fdafd191c7a8599e4ea25496e7731db40ccf29a8
David Wickman <dave@wickmanstudios.com>
2014-12-09 22:09:27 -0500

    Grunt: Add grunt-contrib-clean, grunt-browser-sync; Reorder tasks to have includes run before watch; Watch all current files for changes; Spin up local server that watches all the changes Styles: Remove animate from master page and include in compiled style.css Site: Rename start.html to index.html to allow grunt-browser-sync to spin something up

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,9 @@ module.exports = function(grunt) {
         },
 
         includes: {
-            build: {
+            html: {
                 cwd: 'site', // cwd = current working directory
-                src: [ '*.html', 'templates/*.html' ],
+                src: ['*.html', 'templates/*.html'],
                 dest: 'build/',
         
                 options: {
@@ -26,32 +26,78 @@ module.exports = function(grunt) {
                     includePath: 'includes',
                     banner: '<!-- This is the build file! -->\n'
                 }
+            },
+            img: {
+                src: 'img/*.*',
+                dest: 'build/'
+            },
+            styles: {
+                src: 'css/*.css',
+                dest: 'build/'
+            },
+            js: {
+                src: 'js/*.js',
+                dest: 'build/'
             }
         },
+
         watch: {
-            options: {
-                livereload: true,
+            html: {
+                files: ['site/**/*.html', 'includes/**/*.html' ],
+                tasks: ['includes:html'],
+                options: {
+                    nospawn: true,
+                }
+            },
+            img: {
+                files: ['img/*.*'],
+                tasks: ['includes:img'],
+                options: {
+                    nospawn: true,
+                }
             },
             styles: {
                 files: ['less/**/*.less'], // which files to watch
-                tasks: ['less'],
+                tasks: ['less', 'includes:styles'],
                 options: {
                     nospawn: true
                 }
             },
-            html:{
-                files: ['site/**/*.html', 'includes/**/*.html' ],
-                tasks: ['includes'],
+            js: {
+                files: ['js/*.js'],
+                tasks: ['includes:js'],
                 options: {
                     nospawn: true,
                 }
             }
+        },
 
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: 'build/**'
+                },
+                options: {
+                    watchTask: true,
+                    reloadDelay: 2000,
+                    server: {
+                        baseDir: "build"
+                    }
+                }
+            }
+        },
+
+        clean: {
+            build: 'build',
+            css : 'css'      
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-includes');
-    grunt.registerTask('default', ['less', 'watch', 'includes']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', ['clean', 'less', 'includes', 'browserSync', 'watch']);
 };
