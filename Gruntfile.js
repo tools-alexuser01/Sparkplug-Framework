c8ae40dd0900bd3f3b429d0d7a5c827a6e0fe0fe
WhatsNewSaes <scoelen89@gmail.com>
2014-12-12 22:56:50 -0500

    Revert to 812309d92f87f39c6f072465fb79812acda2978e

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,62 +3,103 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    compress: false, // minifies css if true
+                    compress: false,
                     cleancss: false,
                     optimization: 2,
-                    dumpLineNumbers: 'true'
+                    dumpLineNumbers: 'comments'
                 },
                 files: {
                     // target.css file: source.less file
-                    "css/main.css": "less/main.less"
+                    "_output/css/main.css": "_input/less/main.less"
                 }
             }
         },
+
         includes: {
-            build: {
-                cwd: 'site', // cwd = current working directory
-                src: [ '*.html', 'templates/*.html' ],
-                dest: 'build/',
+            html: {
+                cwd: '_input', // cwd = current working directory
+                src: ['*.html', 'templates/*.html'],
+                dest: '_output/',
         
                 options: {
                     flatten: true,
-                    includePath: 'includes',
-                    banner: '<!-- This is the build file! -->\n'
+                    includePath: '_input/includes',
+                    banner: '<!-- This is the output file! -->\n'
                 }
+            },
+            js: {
+                cwd: '_input',
+                src: 'js/*.js',
+                dest: '_output/'
             }
         },
+
+        copy: {
+            img: {
+                files: [
+                    {expand: true, cwd: '_input', src: ['img/**'], dest: '_output/'}
+                ]
+            }
+        },
+
         watch: {
-            options: {
-                livereload: true,
+            html: {
+                files: ['_input/**/*.html', 'includes/**/*.html' ],
+                tasks: ['includes:html'],
+                options: {
+                    nospawn: true,
+                }
+            },
+            img: {
+                files: ['_input/img/*.*'],
+                tasks: ['copy:img'],
+                options: {
+                    nospawn: true,
+                }
             },
             styles: {
-                files: ['less/**/*.less'], // which files to watch
+                files: ['_input/less/**/*.less'], // which files to watch
                 tasks: ['less'],
                 options: {
                     nospawn: true
                 }
             },
-            html:{
-                files: ['site/**/*.html', 'includes/**/*.html' ],
-                tasks: ['includes'],
+            js: {
+                files: ['_input/js/*.js'],
+                tasks: ['includes:js'],
                 options: {
                     nospawn: true,
                 }
             }
-
         },
-        uncss: {
-            dist: {
-                files: {
-                    'css/main.css': ['build/**/*.html']
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: '_output/**'
+                },
+                options: {
+                    watchTask: true,
+                    reloadDelay: 2000,
+                    server: {
+                        baseDir: "_output"
+                    }
                 }
             }
+        },
+
+        clean: {
+            build: '_output/',
+            css : 'css'      
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-includes');
-    grunt.loadNpmTasks('grunt-uncss');
-    grunt.registerTask('default', ['less', 'watch', 'includes']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', ['clean', 'less', 'includes', 'copy', 'browserSync', 'watch']);
 };
